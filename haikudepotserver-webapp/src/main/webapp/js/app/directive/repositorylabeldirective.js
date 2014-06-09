b3b9505d8c771cf1ee0592c0a8fa14f59f9d506d
Andrew Lindesay <apl@lindesay.co.nz>
2014-06-09 23:17:10 +1200

    + repository label directive to use link function

--- haikudepotserver-webapp/src/main/webapp/js/app/directive/repositorylabeldirective.js
+++ haikudepotserver-webapp/src/main/webapp/js/app/directive/repositorylabeldirective.js
@@ -7,39 +7,87 @@
  * <p>This directive renders a small span which briefly describes the repository.</p>
  */
 
-angular.module('haikudepotserver').directive('repositoryLabel',function() {
-    return {
-        restrict: 'E',
-        templateUrl:'/js/app/directive/repositorylabel.html',
-        replace: true,
-        scope: {
-            repository: '='
-        },
-        controller:
-            ['$scope','$location','userState',
-                function($scope,$location,userState) {
+angular.module('haikudepotserver').directive('repositoryLabel',[
+    '$location',
+    'standardDirectiveMixins','userState',
+    function(
+        $location,
+        standardDirectiveMixins,userState) {
+        return {
+            restrict: 'E',
+            link: function ($scope, element, attributes) {
 
-                    $scope.canView = false;
+                // apply a mixin for standard directive mixins.
+                angular.extend(this, standardDirectiveMixins);
 
-                    $scope.$watch('repository',function(newValue) {
-                        if(!newValue) {
-                            $scope.canView = false;
-                        }
-                        else {
-                            userState.areAuthorized([{
-                                    targetType:'REPOSITORY',
-                                    targetIdentifier:newValue.code,
-                                    permissionCode:'REPOSITORY_VIEW'
-                                }]).then(function(flag) {
-                                $scope.canView = flag;
-                            });
-                        }
-                    });
+                var containerEl = angular.element('<span></span>');
+                var hyperlinkPath = undefined;
+                element.replaceWith(containerEl);
 
-                    $scope.goView = function() {
-                        $location.path('/repository/'+$scope.repository.code).search({});
-                    }
+                var repositoryExpression = attributes['repository'];
+                var shouldLink = attributes['shouldLink'];
+
+                if (!repositoryExpression || !repositoryExpression.length) {
+                    throw 'the repository expression should be defined';
                 }
-            ]
-    };
-});
+
+                $scope.$watch(repositoryExpression, function (repository) {
+
+                    function setupForNoHyperlink() {
+                        if('span' != containerEl[0].tagName.toLowerCase()) {
+                            var el = angular.element('<span></span>');
+                            containerEl.replaceWith(el);
+                            containerEl = el;
+                        }
+
+                        hyperlinkPath = undefined;
+                        containerEl.text(repository ? repository.code : '');
+                    }
+
+                    // we may want an anchor or we may want simple text.
+
+                    if ((undefined == shouldLink || 'true' == shouldLink) && repository && !isChildOfForm(containerEl)) {
+
+                        userState.areAuthorized([{
+                            targetType:'REPOSITORY',
+                            targetIdentifier:repository.code,
+                            permissionCode:'REPOSITORY_VIEW'
+                        }]).then(function(flag) {
+
+                            if(flag) {
+                                if ('a' != containerEl[0].tagName.toLowerCase()) {
+                                    var anchorEl = angular.element('<a href=""></a>');
+                                    containerEl.replaceWith(anchorEl);
+                                    containerEl = anchorEl;
+
+                                    containerEl.on('click', function (el) {
+                                        if (hyperlinkPath) {
+                                            $scope.$apply(function () {
+                                                $location.path(hyperlinkPath).search({});
+                                            });
+                                        }
+                                        el.preventDefault();
+                                    });
+
+                                    containerEl.text(repository ? repository.code : '');
+                                }
+
+                                hyperlinkPath = '/repository/' + repository.code;
+                            }
+                            else {
+                                setupForNoHyperlink();
+                            }
+                        });
+
+                    }
+                    else {
+                        setupForNoHyperlink();
+                    }
+
+
+                });
+
+            }
+        }
+    }
+]);
