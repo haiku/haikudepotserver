be86d13e6de2e2d6319523a63d72b942f9f1bddb
Andrew Lindesay <apl@lindesay.co.nz>
2014-06-30 23:47:18 +1200

    + safety feature to ensure that it is not possible to make too many requests when refreshing tokens

--- haikudepotserver-webapp/src/main/webapp/js/app/service/userstateservice.js
+++ haikudepotserver-webapp/src/main/webapp/js/app/service/userstateservice.js
@@ -21,8 +21,11 @@ angular.module('haikudepotserver').factory('userState',
             jsonRpc,pkgScreenshot,errorHandling,
             constants,referenceData) {
 
-            const SIZE_CHECKED_PERMISSION_CACHE = 25;
+            var SIZE_CHECKED_PERMISSION_CACHE = 25;
+            var SAMPLESIZE_TIMESTAMPS_OF_LAST_TOKEN_RENEWALS = 10;
+            var MIN_MILLIS_FOR_TIMESTAMPS_OF_LAST_TOKEN_RENEWALS = 60 * 1000; // 1 min.
 
+            var timestampsOfLastTokenRenewals = [];
             var tokenRenewalTimeoutPromise = undefined;
 
             var userStateData = {
@@ -47,6 +50,10 @@ angular.module('haikudepotserver').factory('userState',
                     pkgScreenshot.setHeader('Authorization');
                 }
                 else {
+                    if(millisUntilExpirationForToken(token) <= 0) {
+                        throw 'at attempt has been made to set a token that has expired already';
+                    }
+
                     if (userStateData.user && userStateData.user.nickname == tokenNickname(token)) {
                         userStateData.user.token = token;
 
@@ -71,48 +78,83 @@ angular.module('haikudepotserver').factory('userState',
 
             function configureTokenRenewal() {
                 if(userStateData.user && userStateData.user.token) {
-                    function millisUntilExpiration() {
-                        var nowMs = new Date().getTime();
-                        var expMs = tokenExpirationDate(userStateData.user.token).getTime();
-                        return expMs - nowMs;
-                    }
 
-                    var millisUntilRenewal = _.max([0,millisUntilExpiration() * 0.75]);
+                    var millisUntilExpiration = millisUntilExpirationForToken(userStateData.user.token);
+                    var millisUntilRenewal = millisUntilExpiration * 0.75;
 
-                    tokenRenewalTimeoutPromise = $timeout(function () {
-                            if(millisUntilExpiration() < 0) {
-                                $log.info('am going to renew token, but it has already expired');
-                                errorHandling.navigateToError(jsonRpc.errorCodes.AUTHORIZATIONFAILURE); // simulates this happening
-                            }
-                            else {
-                                jsonRpc.call(
-                                    constants.ENDPOINT_API_V1_USER,
-                                    'renewToken',
-                                    [
-                                        { token: userStateData.user.token }
-                                    ]
-                                ).then(
-                                    function (renewTokenResponse) {
-                                        if(renewTokenResponse.token) {
-                                            setToken(renewTokenResponse.token);
-                                            $log.info('did renew the authentication token');
-                                            configureTokenRenewal();
+                    if(millisUntilExpiration > 1000) {
+                        $log.info('will schedule token renewal in ~' + Math.ceil(millisUntilRenewal / 1000) + "s");
+
+                        tokenRenewalTimeoutPromise = $timeout(function () {
+                                if (millisUntilExpirationForToken(userStateData.user.token) < 0) {
+                                    $log.info('am going to renew token, but it has already expired');
+                                    errorHandling.navigateToError(jsonRpc.errorCodes.AUTHORIZATIONFAILURE); // simulates this happening
+                                }
+                                else {
+
+                                    // -------------
+                                    // START : EXCESSIVE TOKEN RENEWAL UPDATE CHECK
+                                    // as a safety measure (just in case) check to make sure that the renewal of the
+                                    // token is not happening too frequently.  This theoretically won't happen.
+
+                                    var nowMs = new Date().getTime();
+
+                                    if(timestampsOfLastTokenRenewals.length = SAMPLESIZE_TIMESTAMPS_OF_LAST_TOKEN_RENEWALS) {
+                                        var firstMs = timestampsOfLastTokenRenewals.shift();
+
+                                        if(nowMs - firstMs < MIN_MILLIS_FOR_TIMESTAMPS_OF_LAST_TOKEN_RENEWALS) {
+                                            throw '10 or more renewals of tokens in < ' + MIN_MILLIS_FOR_TIMESTAMPS_OF_LAST_TOKEN_RENEWALS + 'ms -- something wrong; failing';
                                         }
-                                        else {
-                                            $log.info('was not able to renew authentication token');
-                                            errorHandling.navigateToError(jsonRpc.errorCodes.AUTHORIZATIONFAILURE); // simulates this happening
-                                        }
-                                    },
-                                    function (err) {
-                                        $log.info('failure to renew the authentication token');
-                                        errorHandling.handleJsonRpcError(err);
                                     }
-                                );
-                            }
-                        },
-                        millisUntilRenewal
-                    );
+
+                                    timestampsOfLastTokenRenewals.push(nowMs);
+
+                                    // END : EXCESSIVE TOKEN RENEWAL UPDATE CHECK
+                                    // -------------
+
+                                    jsonRpc.call(
+                                        constants.ENDPOINT_API_V1_USER,
+                                        'renewToken',
+                                        [
+                                            { token: userStateData.user.token }
+                                        ]
+                                    ).then(
+                                        function (renewTokenResponse) {
+                                            if (renewTokenResponse.token) {
+                                                setToken(renewTokenResponse.token);
+                                                $log.info('did renew the authentication token');
+                                                configureTokenRenewal();
+                                            }
+                                            else {
+                                                $log.info('was not able to renew authentication token');
+                                                errorHandling.navigateToError(jsonRpc.errorCodes.AUTHORIZATIONFAILURE); // simulates this happening
+                                            }
+                                        },
+                                        function (err) {
+                                            $log.info('failure to renew the authentication token');
+                                            errorHandling.handleJsonRpcError(err);
+                                        }
+                                    );
+                                }
+                            },
+                            millisUntilRenewal
+                        );
+                    }
+                    else {
+                        $log.warn('will not schedule token renewal as the token has ether already expired or is about to');
+                    }
                 }
+            }
+
+            /**
+             * <p>Returns the number of milliseconds until the expiration date of the supplied token from now.  If
+             * the token has already expired then this function will return a negative value.</p>
+             */
+
+            function millisUntilExpirationForToken(token) {
+                var nowMs = new Date().getTime();
+                var expMs = tokenExpirationDate(token).getTime();
+                return expMs - nowMs;
             }
 
             function tokenClaimSet(token) {
@@ -144,6 +186,11 @@ angular.module('haikudepotserver').factory('userState',
                 return new Date(claimSet.exp * 1000);
             }
 
+            /**
+             * <p>The token is a 'json web token' that contains a 'subject'.  In the case of this application,
+             * the subject contains the nickname of the user.</p>
+             */
+
             function tokenNickname(token) {
                 var claimSet = tokenClaimSet(token);
 
@@ -160,7 +207,6 @@ angular.module('haikudepotserver').factory('userState',
 
                 return sub.substring(0,suffixIndex);
             }
-
 
             // ------------------------------
             // USER
