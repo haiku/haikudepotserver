3a60d3914d7fec9012df2ddd0820455886f71b4c
Andrew Lindesay <apl@lindesay.co.nz>
2015-05-01 22:56:34 +1200

    changes to fallback controller so it can deliver a favicon

--- haikudepotserver-webapp/src/main/java/org/haikuos/haikudepotserver/support/web/FallbackController.java
+++ haikudepotserver-webapp/src/main/java/org/haikuos/haikudepotserver/support/web/FallbackController.java
@@ -6,13 +6,18 @@
 package org.haikuos.haikudepotserver.support.web;
 
 import com.google.common.base.Optional;
+import com.google.common.base.Preconditions;
+import com.google.common.io.ByteStreams;
 import com.google.common.net.HttpHeaders;
+import net.jawr.web.resource.bundle.factory.util.ServletContextAware;
 import org.apache.cayenne.ObjectContext;
 import org.apache.cayenne.configuration.server.ServerRuntime;
+import org.haikuos.haikudepotserver.dataobjects.MediaType;
 import org.haikuos.haikudepotserver.dataobjects.PkgVersion;
 import org.haikuos.haikudepotserver.pkg.PkgOrchestrationService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,8 +27,10 @@ import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.annotation.Resource;
+import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.PrintWriter;
 
 /**
@@ -32,12 +39,13 @@ import java.io.PrintWriter;
  */
 
 @Controller
-//@RequestMapping("/wwo")
 public class FallbackController {
 
     protected static Logger LOGGER = LoggerFactory.getLogger(PkgOrchestrationService.class);
 
     public final static String KEY_TERM = "term";
+
+    public final static String VALUE_FAVICON = "favicon";
 
     @Resource
     private ServerRuntime serverRuntime;
@@ -48,6 +56,9 @@ public class FallbackController {
     @Value("${baseurl}")
     private String baseUrl;
 
+    @Autowired
+    private ServletContext servletContext;
+
     private String termDebug(String term) {
         if(term.length() > 64) {
             return term.substring(0,64) + "...";
@@ -56,39 +67,58 @@ public class FallbackController {
         return term;
     }
 
+    private void streamFavicon(HttpServletResponse response) throws IOException {
+        Preconditions.checkState(null!=servletContext, "the servlet context must be supplied");
+        response.setContentType(com.google.common.net.MediaType.ICO.toString());
+
+        try (InputStream inputStream = servletContext.getResourceAsStream("/img/favicon.ico")) {
+            if(null==inputStream) {
+                throw new IllegalStateException("unable to find the favicon resource");
+            }
+
+            ByteStreams.copy(inputStream, response.getOutputStream());
+        }
+    }
+
     @RequestMapping(value = "/{"+KEY_TERM+"}", method = RequestMethod.GET)
     public void fallback(
             HttpServletResponse response,
             @PathVariable(value = KEY_TERM) String term)
-    throws IOException {
+            throws IOException {
 
         ObjectContext context = serverRuntime.getContext();
-        Optional<PkgVersion> pkgVersionOptional = pkgOrchestrationService.getLatestPkgVersionForPkgName(
-                context,
-                term);
 
-        if(pkgVersionOptional.isPresent()) {
-            UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(baseUrl).pathSegment("#", "pkg");
-            pkgVersionOptional.get().appendPathSegments(builder);
-            UriComponents uriComponents = builder.build();
-
-            response.setStatus(HttpServletResponse.SC_MOVED_TEMPORARILY);
-            response.setHeader(HttpHeaders.LOCATION, uriComponents.toUriString());
-
-            PrintWriter w = response.getWriter();
-            w.format("redirecting to; %s", uriComponents.toUriString());
-            w.flush();
-
-            LOGGER.info("did redirect to a package for; {}", term);
+        if(term.equalsIgnoreCase(VALUE_FAVICON)) {
+            streamFavicon(response);
         }
         else {
-            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
 
-            PrintWriter w = response.getWriter();
-            w.format("unable to find an entity for; %s", termDebug(term));
-            w.flush();
+            Optional<PkgVersion> pkgVersionOptional = pkgOrchestrationService.getLatestPkgVersionForPkgName(
+                    context,
+                    term);
 
-            LOGGER.info("did not find a package for; {}", termDebug(term));
+            if (pkgVersionOptional.isPresent()) {
+                UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(baseUrl).pathSegment("#", "pkg");
+                pkgVersionOptional.get().appendPathSegments(builder);
+                UriComponents uriComponents = builder.build();
+
+                response.setStatus(HttpServletResponse.SC_MOVED_TEMPORARILY);
+                response.setHeader(HttpHeaders.LOCATION, uriComponents.toUriString());
+
+                PrintWriter w = response.getWriter();
+                w.format("redirecting to; %s", uriComponents.toUriString());
+                w.flush();
+
+                LOGGER.info("did redirect to a package for; {}", term);
+            } else {
+                response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+
+                PrintWriter w = response.getWriter();
+                w.format("unable to find an entity for; %s", termDebug(term));
+                w.flush();
+
+                LOGGER.info("did not find a package for; {}", termDebug(term));
+            }
         }
 
     }
