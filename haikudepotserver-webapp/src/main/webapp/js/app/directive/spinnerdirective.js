b039ca4ff2eee752a0f2e45ed7c411a1e77e6a54
Andrew Lindesay <apl@lindesay.co.nz>
2015-10-20 23:48:40 +1300

    SVG to use CSS animations rather than SMIL animations as this has been deprecated.

--- haikudepotserver-webapp/src/main/webapp/js/app/directive/spinnerdirective.js
+++ haikudepotserver-webapp/src/main/webapp/js/app/directive/spinnerdirective.js
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013, Andrew Lindesay
+ * Copyright 2013-2015, Andrew Lindesay
  * Distributed under the terms of the MIT License.
  */
 
@@ -10,40 +10,80 @@
  * operation is being undertaken.</p>
  */
 
-angular.module('haikudepotserver').directive('spinner',function() {
-    return {
-        restrict: 'E',
-        templateUrl:'/js/app/directive/spinner.html',
-        replace: true,
-        scope: {
-            spin: '='
-        },
-        controller:
-            ['constants','$scope','$timeout',
-                function(constants,$scope,$timeout) {
+angular.module('haikudepotserver').directive('spinner',[
+        '$timeout','constants',
+        function($timeout,constants) {
+            return {
+                restrict: 'E',
+                link : function($scope,element,attributes) {
 
+                    var spinExpr = attributes['spin'];
                     var timeoutHandle = undefined;
 
-                    $scope.delayedSpin = false;
+                    var containerEl = angular.element('<div id="spinner-container" class="modal-backdrop-container"></div>');
+                    var innerContainerEl = angular.element('<div></div>');
+                    containerEl.append(innerContainerEl);
+                    element.replaceWith(containerEl);
 
-                    $scope.$watch('spin', function(newSpin) {
+                    function setupSpin(flag) {
+                        if(flag) {
+
+                            if(Modernizr.svg) {
+                                // blend in the SVG
+
+                                var ns = "http://www.w3.org/2000/svg";
+                                var svgEl = angular.element('<svg width="120" height="24" class="spinner-img"></svg>');
+
+                                for (var i = 0; i < 3; i++) {
+                                    var circle = document.createElementNS(ns, "circle");
+                                    circle.setAttributeNS(null, 'cy', '' + 12);
+                                    circle.setAttributeNS(null, 'cx', '' + (12 + (i * 48)));
+                                    circle.setAttributeNS(null, 'r', '' + 11);
+                                    circle.setAttributeNS(null, 'class', 'spinner-img-c' + i);
+                                    svgEl[0].appendChild(circle);
+                                }
+
+                                innerContainerEl.append(svgEl);
+                            }
+                            else {
+                                innerContainerEl.append(angular.element('<img src="img/spinner.gif">'));
+                            }
+
+                            containerEl.removeClass('app-hide');
+                        }
+                        else {
+                            innerContainerEl.empty();
+                            containerEl.addClass('app-hide');
+                        }
+                    }
+
+                    // this will setup a timer so that there is a slight delay before the spinner appears.  This
+                    // will prevent the spinner from flashing on the screen.
+
+                    $scope.$watch(spinExpr, function(newValue) {
+
                         if(timeoutHandle) {
                             $timeout.cancel(timeoutHandle);
                             timeoutHandle = undefined;
                         }
 
-                        if(!newSpin) {
-                            $scope.delayedSpin = false;
+                        if(newValue) {
+                            timeoutHandle = $timeout(
+                                function() {
+                                    timeoutHandle = undefined;
+                                    setupSpin(true);
+                                },
+                                constants.DELAY_SPINNER
+                            );
                         }
                         else {
-                            timeoutHandle = $timeout(function() {
-                               $scope.delayedSpin = true;
-                            },
-                            constants.DELAY_SPINNER);
+                            setupSpin(false);
                         }
-                    })
+
+                    });
 
                 }
-            ]
-    };
-});
+
+            };
+        }]
+);
--- haikudepotserver-webapp/src/main/webapp/js/app/directive/supporteddirective.js
+++ haikudepotserver-webapp/src/main/webapp/js/app/directive/supporteddirective.js
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014, Andrew Lindesay
+ * Copyright 2014-2015, Andrew Lindesay
  * Distributed under the terms of the MIT License.
  */
 
@@ -10,8 +10,8 @@
  */
 
 angular.module('haikudepotserver').directive('supported',[
-    '$log',
-    function($log) {
+    // no injections
+    function() {
         return {
             restrict: 'E',
             link : function($scope,element,attributes) {
