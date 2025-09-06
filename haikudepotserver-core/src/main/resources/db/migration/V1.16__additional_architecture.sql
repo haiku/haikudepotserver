52baf75008afe5bf37bf214837c9d6be57b380bb
Andrew Lindesay <apl@lindesay.co.nz>
2025-09-06 19:06:10 +1200

    add riscv64 and arm64 architectures

---
+++ haikudepotserver-core/src/main/resources/db/migration/V1.16__additional_architecture.sql
@@ -0,0 +1,2 @@
+INSERT INTO haikudepot.architecture VALUES ('arm64', NEXTVAL('haikudepot.architecture_seq'));
+INSERT INTO haikudepot.architecture VALUES ('riscv64', NEXTVAL('haikudepot.architecture_seq'));
