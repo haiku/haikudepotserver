8772bf8eb9b5302ec19e727155ced1840bdd079c
Andrew Lindesay <apl@lindesay.co.nz>
2025-11-10 09:11:24 +1300

    improved logging for jobs

--- haikudepotserver-core/src/main/java/org/haiku/haikudepotserver/job/DbDistributedJobServiceImpl.java
+++ haikudepotserver-core/src/main/java/org/haiku/haikudepotserver/job/DbDistributedJobServiceImpl.java
@@ -293,14 +293,21 @@ public class DbDistributedJobServiceImpl extends AbstractExecutionThreadService 
         transactionTemplate.executeWithoutResult((transactionStatus) -> {
             // some started jobs may have been forcefully stopped (eg; JVM stopped) and the started job would be left
             // dangling. This will ensure that those jobs are marked as failed.
+
+            LOGGER.info("will correct dangling started jobs");
             correctDanglingStartedJobs();
+            LOGGER.info("did correct dangling started jobs");
+
             // clear any jobs which are completed and have naturally expired.
+            LOGGER.info("will clear completed expired jobs");
             long clearedCompletedExpiredJobs = jpaJobService.clearCompletedExpiredJobs(clock.instant());
+            LOGGER.info("did clear {} completed expired jobs", clearedCompletedExpiredJobs);
+
             // Some jobs may be still in the database but have failed owing to system reasons. This will clean those
             // up as well.
+            LOGGER.info("will clear expired jobs");
             long clearedExpiredJobs = jpaJobService.clearExpiredJobs(clock.instant().minus(TTL_MAX, ChronoUnit.MILLIS));
-
-            LOGGER.info("cleared {} expired jobs", clearedCompletedExpiredJobs + clearedExpiredJobs);
+            LOGGER.info("did clear {} expired jobs", clearedCompletedExpiredJobs);
         });
     }
 
