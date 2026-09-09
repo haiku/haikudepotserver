1ad999ebb8d07aeb20e857551c70077506a9782e
Andrew Lindesay <apl@lindesay.co.nz>
2026-09-09 20:58:18 +1200

    fixing tests

--- haikudepotserver-core-test/src/main/java/org/haiku/haikudepotserver/IntegrationTestSupportService.java
+++ haikudepotserver-core-test/src/main/java/org/haiku/haikudepotserver/IntegrationTestSupportService.java
@@ -379,7 +379,7 @@ public class IntegrationTestSupportService {
         UserUsageConditionsAgreement agreement = context.newObject(UserUsageConditionsAgreement.class);
         agreement.setUser(user);
         agreement.setTimestampAgreed();
-        agreement.setUserUsageConditions(UserUsageConditions.getByCode(context, "UUC2024V01"));
+        agreement.setUserUsageConditions(UserUsageConditions.getByCode(context, "UUC2026V01"));
         context.commitChanges();
     }
 
